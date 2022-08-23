3afc6394fc105d7a6c8e0ac14d6b9a6358e75af3
safwansadi <safwaniftekher@gmail.com>
2022-08-24 01:29:13 +0600

    try

--- src/services/product.js
+++ src/services/product.js
@@ -2,29 +2,70 @@
 
 const db = require("../models/");
 const { Sequelize } = db;
-const { Op, QueryTypes } = Sequelize;
+const { Op } = Sequelize;
 const logger = require("../utils/logger");
 const _ = require("lodash");
 const BaseService = require("./base-service");
 const tag = "services/product.js";
-
 class Product extends BaseService {
   constructor() {
     super("product");
   }
+
+  getProductsForSearchV2(keywords, result) {
+    return new Promise((resolve) => {
+      const tempData = [];
+      for (let i = 0; i < _.size(result); i++) {
+        const resultTags = _.split(result[i].name, " ");
+        // console.log('result name', resultTags);
+        for (let j = 0; j < _.size(resultTags); j++) {
+          // console.log('trimmed', resultTags[j]);
+          const words = _.split(resultTags[j].toLowerCase(), " ");
+
+          // console.log('words', words);
+
+          const keywordsMatchCount = _.reduce(
+            keywords,
+            (result, value) => {
+              if (_.find(words, (word) => word.startsWith(value.toLowerCase())))
+                return result + 1;
+
+              return result;
+            },
+            0
+          );
+
+          console.log("keywordsMatchCount", keywordsMatchCount);
+
+          // if (_.isEqual(keywordsMatchCount, 0)) continue;
+
+          tempData.push({
+            product: result[i],
+            // sentence: trimmedResultTag,
+            wordsCount: _.size(words),
+            keywordsMatchCount,
+          });
+
+          // console.log(tempData['keywordsMatchCount']);
+        }
+      }
+
+      resolve(
+        _.uniqBy(
+          _.map(
+            _.reverse(_.sortBy(tempData, ["keywordsMatchCount"])),
+            (datum) => this.setProductData(datum.product.toJSON())
+          ),
+          "id"
+        )
+      );
+    });
+  }
+
   async searchProduct(queries) {
     try {
       const key = queries.key;
       const keywords = _.split(queries.key, " ");
-
-      const sort = [
-        [
-          Sequelize.literal(
-            `CASE WHEN tags like ' %${key}%' THEN 1 ELSE 2 END`
-          ),
-          "ASC",
-        ],
-      ];
 
       let page = queries.page && parseInt(queries.page);
       let limit = queries.page && queries.limit && parseInt(queries.limit);
@@ -55,14 +96,21 @@ class Product extends BaseService {
           status: "active",
         },
         null,
-        // sort,
+        null,
         offset,
         limit
       );
 
-      return { success: true, data: products };
+      const formattedProducts = await this.getProductsForSearchV2(
+        keywords,
+        products,
+        !queries.price ? null : _.split(queries.price, "-"),
+        !queries.brand ? null : _.split(queries.brand, ",")
+      );
+
+      return { success: true, data: formattedProducts };
     } catch (error) {
-      logger.error(tag + ": searchProductForCustomerV2", error);
+      logger.error(tag + ": searchProductForCustomer", error);
 
       return { success: false, data: error };
     }
