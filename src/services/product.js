4b376d9a1665c152ae8013e3c1476722b6177c5f
safwansadi <safwaniftekher@gmail.com>
2022-08-23 13:13:26 +0600

    init

--- src/services/product.js
+++ src/services/product.js
@@ -2,32 +2,58 @@
 
 const db = require("../models/");
 const { Sequelize } = db;
-const { Op, QueryTypes } = Sequelize;
+const { Op } = Sequelize;
 const logger = require("../utils/logger");
 const _ = require("lodash");
 const BaseService = require("./base-service");
 const tag = "services/product.js";
-
 class Product extends BaseService {
   constructor() {
     super("product");
   }
+
   async searchProduct(queries) {
     try {
       const key = queries.key;
-      const keywords = key.includes(" ") ? _.split(queries.key, " ") : [key];
+      const keywords = _.split(queries.key, " ");
 
-      let sort;
-
-      if (queries.sort) {
-        sort = queries.sort.includes(" ")
-          ? [_.split(queries.sort, " ")]
-          : [[queries.sort, "ASC"]];
-      }
+      // const sort = [
+      //   [
+      //     Sequelize.literal(
+      //       `CASE WHEN tags like ' %${key}%' THEN 1 ELSE 2 END`
+      //     ),
+      //     "desc",
+      //   ],
+      // ];
 
       let page = queries.page && parseInt(queries.page);
       let limit = queries.page && queries.limit && parseInt(queries.limit);
       let offset = queries.page && queries.limit && page * limit - limit;
+
+      const productsExactMatch = await super.readByWhere(
+        [
+          "id",
+          "slug",
+          "name",
+          "nameBn",
+          "tags",
+          "retailPrice",
+          "discountPrice",
+          "quantity",
+        ],
+        {
+          [Op.or]: [
+            Sequelize.where(Sequelize.fn("lower", Sequelize.col("tags")), {
+              [Op.substring]: key.toLowerCase().trim(),
+            }),
+          ],
+          status: "active",
+        },
+        null,
+        null,
+        offset,
+        limit
+      );
 
       const products = await super.readByWhere(
         [
@@ -42,6 +68,9 @@ class Product extends BaseService {
         ],
         {
           [Op.or]: [
+            Sequelize.where(Sequelize.fn("lower", Sequelize.col("tags")), {
+              [Op.substring]: key.toLowerCase().trim(),
+            }),
             ..._.map(keywords, (keyword) =>
               Sequelize.where(Sequelize.fn("lower", Sequelize.col("tags")), {
                 [Op.substring]: keyword.toLowerCase().trim(),
@@ -51,12 +80,19 @@ class Product extends BaseService {
           status: "active",
         },
         null,
-        sort,
+        null,
         offset,
         limit
       );
 
-      return { success: true, data: products };
+      let result = {
+        productsExactMatch,
+        products,
+      };
+
+      console.log("product", products);
+
+      return { success: true, data: result };
     } catch (error) {
       logger.error(tag + ": searchProductForCustomerV2", error);
 
